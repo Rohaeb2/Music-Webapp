6093e633d6e8e3267ff2cb286272a064a63920d0
Rohaeb2 <rohaebkali@gmail.com>
2026-05-30 15:25:27 -0700

    fixed UI styling for Home Page

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.use(express.static('public'));
 app.use(express.urlencoded({extended: true }))
 
 app.get('/',(req,res) =>{
-    res.send('Hello World')
+    res.sendFile(path.join(__dirname,'','views','home.html'))
 });
 
 app.use('/auth', AuthRouters)
