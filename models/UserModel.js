d031a2fe0bae7dd49e8c58e32534a1c3d9bb3a35
Rohaeb2 <rohaebkali@gmail.com>
2026-06-13 18:31:12 -0700

    database mode for user data

--- models/UserModel.js
+++ models/UserModel.js
@@ -1,11 +1,12 @@
 const path = require('path')
 require('dotenv').config()
 const pool = require("../config/database")
+const User = require("../models/User")
 
 class UserModel{
     constructor(){
     }
-    static async verifyUser(username){
+    async verifyUser(username){
         try{
             const sql = 'SELECT * FROM users WHERE username = ? LIMIT 1';
             const [rows] = await pool.query(sql, [username])
@@ -20,6 +21,22 @@ class UserModel{
             console.log(error)
         }
     }
+    async getUser(id){
+        try{
+            const sql = 'SELECT * FROM users WHERE id = ? LIMIT 1';
+            const [rows] = await pool.query(sql, [id])
+            if (rows.length === 0){
+                console.log("failed")
+                return none
+            } else {
+                const user = [rows][0]
+                const loggedInUser = new User(user)
+                return  loggedInUser
+            }
+        } catch (error){
+            console.log(error)
+    }
+    } 
 }
 
-module.exports = UserModel;
+module.exports = new UserModel;
