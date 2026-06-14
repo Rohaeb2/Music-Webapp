3a591ac20227a04755239c61f774052c96380563
Rohaeb2 <rohaebkali@gmail.com>
2026-06-13 18:24:05 -0700

    added ejs and spotify routes

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const path = require('path')
 const app = express();
 const port = 3000;
 const AuthRouters = require("./routes/auth.js")
+const SpotifyRouters = require("./routes/spotify.js")
 const DashboardRouters = require("./routes/dashboard.js")
 const session = require('express-session')
 
@@ -15,19 +16,18 @@ app.use(session({
     resave: false,
     saveUninitialized: false,
     cookie: {
-        secure: true,
+        secure: false,
         httpOnly: true,
         maxAge: 24 * 60 * 60 *1000
     }
 }))
-
+app.set('view engine', 'ejs')
 app.get('/',(req,res) =>{
-    req.session.name = "session"
-    console.log(session.name)
     res.sendFile(path.join(__dirname,'','views','home.html'))
 });
 
 app.use('/auth', AuthRouters)
+app.use('/spotify',SpotifyRouters)
 app.use('/Wavelength', DashboardRouters)
 
 
