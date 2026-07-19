758e32e09789fe2f7f53a7cb6f343ccd89d459a2
Rohaeb2 <rohaebkali@gmail.com>
2026-07-19 16:22:20 -0400

    add profile and top tracks routes

--- routes/spotify.js
+++ routes/spotify.js
@@ -6,6 +6,6 @@ const isLoggedIn = require('../middleware/authMiddleware')
 router.get('/connect',isLoggedIn,spotifyController.connectSpotify);
 router.get('/callback',isLoggedIn,spotifyController.getSpotifyData);
 router.get('/data',isLoggedIn,spotifyController.showSpotifyData);
-
-
+router.get('/profile',isLoggedIn,spotifyController.getProfile);
+router.get('/top-tracks',isLoggedIn,spotifyController.showTopTracks)
 module.exports = router;
